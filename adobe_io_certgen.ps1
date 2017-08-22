9e1be62362ebc37e23e0930c9979ee7011f28128
Kevin Bhunut <bhunut@adobe.com>
2017-08-22 10:47:33 -0700

    Added Utils Folder, OpenSSL cert gen script

---
+++ adobe_io_certgen.ps1
@@ -0,0 +1,74 @@
+﻿$ErrorActionPreference = "Stop"
+
+Write-Host "Generate Adobe.IO Self-Signed Certifcation"
+$defaulExpirationDate = (Get-Date).AddYears(5).ToString("d")
+$prompt = Read-Host -Prompt "Enter Certificate Expiring Date [$defaulExpirationDate]" 
+[datetime]$date = ($defaulExpirationDate,$prompt)[[bool]$prompt]
+$expirationDay = ($date - (Get-Date)).Days
+$USTFolder = Get-USTFolder
+$OpenSSL = "$USTFolder\Utils\openSSL\openssl.exe"
+
+if(Test-Path $OpenSSL){
+    $argslist = @("/c $OpenSSL",'req',
+                '-x509',
+                '-sha256',
+                '-nodes',
+                "-days $expirationDay",
+                "-newkey rsa`:2048",
+                "-keyout $USTFolder\private.key",
+                "-out $USTFolder\certificate_pub.crt")
+
+    $process = Start-Process -FilePath cmd.exe -ArgumentList $argslist -PassThru -Wait
+    if($process.ExitCode -eq 0){
+        Write-Host "Completed - Certificate located in $USTFolder."
+        Pause
+        & explorer.exe "https://www.adobe.io/console"
+    }else{
+        Write-Error "Error Generating Certificate"
+    }
+
+}else{
+
+    Write-Error "Unable to Locate $OpenSSL"
+
+}
+
+function Get-USTFolder{
+    param(
+
+        $USTFolder = "$env:SystemDrive\user_sync"
+    )
+ 
+    if(Test-Path $USTFolder){
+
+        return $USTFolder
+
+    }else{
+        $selectedFolder = Get-Folder
+        if($selectedFolder){
+            return Get-USTFolder -USTFolder $selectedFolder
+        }
+    }
+
+}
+
+
+Function Get-Folder()
+
+{
+    Add-Type -AssemblyName System.Windows.Forms
+
+    $foldername = New-Object System.Windows.Forms.FolderBrowserDialog
+    $form = New-Object System.Windows.Forms.Form -Property @{TopMost = $True}
+    $foldername.Description = "Select User-Sync-Tool folder:"
+    $foldername.rootfolder = "MyComputer"
+    $foldername.ShowNewFolderButton = $false
+
+    if($foldername.ShowDialog($form) -eq "OK")
+    {
+        $folder += $foldername.SelectedPath
+    }
+    return $folder
+}
+
+
--- install.ps1
+++ install.ps1
@@ -35,12 +35,43 @@ function Expand-Targz {
    Start-Process cmd.exe -ArgumentList ("/c $7zipTempPath\7za.exe x $Path -so  | $7zipTempPath\7za.exe x -y -si -ttar -o`"$OutPut`"") -Wait 
 
 }
+function Show-SaveDialog(){
+    Add-Type -AssemblyName System.Windows.Forms
+    Add-Type -AssemblyName PresentationCore,PresentationFramework
 
-if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
-{
+    $foldername = New-Object System.Windows.Forms.FolderBrowserDialog
+    $form = New-Object System.Windows.Forms.Form -Property @{TopMost = $True}
+    $foldername.Description = "Select User-Sync-Tool installation folder:"
+    $foldername.rootfolder = "MyComputer"
+
+    if($foldername.ShowDialog($form) -eq "OK")
+    {
+        $folder = $foldername.SelectedPath
+        if((Get-ChildItem -Recurse $folder).Count -gt 0){
+
+            $ButtonType = [System.Windows.MessageBoxButton]::YesNo
+            $MessageboxTitle = "Warning!"
+            $Messageboxbody = “Selected Folder is not empty! Are you sure you want to continue?”
+            $MessageIcon = [System.Windows.MessageBoxImage]::Warning
+            $result = [System.Windows.MessageBox]::Show($Messageboxbody,$MessageboxTitle,$ButtonType,$messageicon)
+
+            if($result -ne "Yes"){
+                Write-Error "UST Installation aborted"
+            }
+    
+        }
+
+    }else{
+        Write-Error "UST Installation aborted"
+    }
+    return $folder
+}
+
+if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)){
     Write-Host "Elevated."
     $DownloadFolder = "$env:TEMP\USTDownload"
-    $USTFolder = "$env:SystemDrive\user_sync"
+    $USTFolder = Show-SaveDialog
+
     $DownloadedItem = @()
 
     #Python Install
@@ -69,9 +100,6 @@ if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsI
     #Set Environment Variable
 
     [Environment]::SetEnvironmentVariable("PEX_ROOT", "$env:SystemDrive\PEX", "Machine")
-
-    #Create UST Folder
-    New-Item -Path $USTFolder -ItemType 'Directory' -Force | Out-Null
     
     #Download UST 2.2 and Extract
     $USTdownloadList = @()
@@ -91,15 +119,54 @@ if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsI
     }
 
     
-    #Copy and make it readable in windows - "config files - basic" in example to root
-    $configBasicPath = "$USTFolder\examples\config files - basic"
-    if(Test-Path -Path $configBasicPath){
-            
-        (Get-Content "$configBasicPath\1 user-sync-config.yml") | Set-Content "$USTFolder\user-sync-config.yml" -Force -Verbose
-        (Get-Content "$configBasicPath\2 connector-umapi.yml") | Set-Content "$USTFolder\connector-umapi.yml" -Force -Verbose
-        (Get-Content "$configBasicPath\3 connector-ldap.yml") | Set-Content "$USTFolder\connector-ldap.yml" -Force -Verbose
+    #Make example config files readable in windows and Copy "config files - basic" to root
+    $configExamplePath = "$USTFolder\examples"
+    if(Test-Path -Path $configExamplePath){    
+        Get-ChildItem -Path $configExamplePath -Recurse -Filter '*.yml' | % { ( $_ |  Get-Content ) | Set-Content $_.pspath -Force }
+        #Copy config files
+        $configBasicPath = "$configExamplePath\config files - basic"
+        Copy-Item -Path "$configBasicPath\3 connector-ldap.yml" -Destination $USTFolder\connector-ldap.yml -Force
+        Copy-Item -Path "$configBasicPath\2 connector-umapi.yml" -Destination $USTFolder\connector-umapi.yml -Force
+        Copy-Item -Path "$configBasicPath\1 user-sync-config.yml" -Destination $USTFolder\user-sync-config.yml -Force
 
     }
+
+    #Download OpenSSL 1.0.2l binary for Windows and extract to utils folder
+    $openSSLBinURL = "https://indy.fulgan.com/SSL/openssl-1.0.2l-x64_86-win64.zip"
+    $openSSLBinFileName = $openSSLBinURL.Split('/')[-1]
+    $openSSLOutputPath = "$DownloadFolder\$openSSLBinFileName"
+    $openSSLUSTFolder = "$USTFolder\Utils\openSSL"
+    Write-Host "Downloading Python from $openSSLBinURL"
+    Invoke-WebRequest -Uri $openSSLBinURL -OutFile $openSSLOutputPath
+
+    if(Test-Path $openSSLOutputPath){
+        #Extracting downloaded file to UST folder.
+        Write-Host "Extracting $openSSLBinFileName to $openSSLUSTFolder"
+        try{
+            New-Item -Path $openSSLUSTFolder -ItemType Directory -Force
+            [System.IO.Compression.ZipFile]::ExtractToDirectory($openSSLOutputPath, $openSSLUSTFolder)
+            Write-Host "Completed extracting $openSSLBinFileName to $openSSLUSTFolder"
+        }catch{
+            
+            Write-Error "Unable to extract openSSL"
+        }
+    }
+
+    #Download Adobe.IO Cert generation Script and put it into utils\openSSL folder
+    $adobeIOCertScriptURL = "https://raw.githubusercontent.com/bhunut-adobe/user-sync-quick-install/master/adobe_io_certgen.ps1"
+    $adobeIOCertScript = $openSSLBinURL.Split('/')[-1]
+    $adobeIOCertScriptOutputPath = "$USTFolder\Utils\openSSL\$openSSLBinFileName"
+    Write-Host "Downloading Adobe.IO Cert Generation Script from $adobeIOCertScriptURL"
+    Invoke-WebRequest -Uri $adobeIOCertScriptURL -OutFile $adobeIOCertScriptOutputPath
+
+    if(Test-Path $adobeIOCertScriptOutputPath){
+        
+       $batchfile = '@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -file ' + $adobeIOCertScriptOutputPath
+
+       $batchfile | Out-File "$openSSLUSTFolder\Adobe_IO_Cert_Generation.bat" -Force
+        
+    }
+
 
     #Delete Temp DownloadFolder for UST, Python and Config files
     Remove-Item -Path $DownloadFolder -Recurse -Confirm:$false -Force -Verbose
@@ -112,12 +179,8 @@ if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsI
     #Open UST Install Folder
 
     & explorer.exe $USTFolder
-}
-
-else
-{
+}else{
     Write-host "Not elevated. Rerun the script with elevated permission"
 }
-
 
 
